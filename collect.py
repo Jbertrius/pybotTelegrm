e7b18e6b6a4faf88c3b8c9f82b704598b5fe8029
yr28057 <eyram-jean-bertrand.ezin-ext@yrnet.net>
2019-09-13 15:28:13 +0200

    make change

--- collect.py
+++ collect.py
@@ -15,36 +15,42 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.kick_chat_member(message.chat.id, message.from_user.id)
-    unique_code = extract_unique_code(message.text)
     bot.reply_to(message, """\
 Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say {0} the exact same thing to you!\
-""".format(unique_code))
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
 
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-# @bot.message_handler(commands=['help', 'start'])
-# def echo_message(message):
-#     # bot.kick_chat_member('933543664', message.from_user.id)
-#     bot.reply_to(message, message.text)
+# Handle '/add'
+@bot.message_handler(commands=['add'])
+def send_welcome(message):
+    import logging
+    logging.info('ezr')
+    bot.reply_to(message, """\
+            Hi there, I am EchoBot.
+            I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+            """)
 
-def extract_unique_code(text):
-    # Extracts the unique_code from the sent /start command.
-    return text.split()[1] if len(text.split()) > 1 else None
-
-def in_storage(unique_code):
-    # Should check if a unique code exists in storage
-    return True
-
-def get_username_from_storage(unique_code):
-    # Does a query to the storage, retrieving the associated username
-    # Should be replaced by a real database-lookup.
-    return "ABC" if in_storage(unique_code) else None
-
-def save_chat_id(chat_id, username):
-    # Save the chat_id->username to storage
-    # Should be replaced by a real database query.
-    pass
+# def extract_unique_code(text):
+#     # Extracts the unique_code from the sent /start command.
+#     return text.split()[1] if len(text.split()) > 1 else None
+#
+#
+# def in_storage(unique_code):
+#     # Should check if a unique code exists in storage
+#     return True
+#
+#
+# def get_username_from_storage(unique_code):
+#     # Does a query to the storage, retrieving the associated username
+#     # Should be replaced by a real database-lookup.
+#     return "ABC" if in_storage(unique_code) else None
+#
+#
+# def save_chat_id(chat_id, username):
+#     # Save the chat_id->username to storage
+#     # Should be replaced by a real database query.
+#     pass
 
 
 bot.infinity_polling(True)
